07fe87aab7f2bac50dc07243b0901ee3f3652fae
John Kine <jkine00@john-kines-mbp.home>
2016-03-08 20:44:50 +0000

    Initial Commit

---
+++ Pokedex2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex2
+//
+//  Created by John Kine on 2016-03-08.
+//  Copyright © 2016 John Kine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
